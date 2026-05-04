318ade5ae14531b0b8cdfa70390ab6df4faafdb5
vincenzobucciero <vincenzo.bucciero001@studenti.uniparthenope.it>
2026-05-04 15:29:31 +0200

    feat: Set up L19SecurityModelDemo project with initial structure, UI components, and resources

---
+++ ta_exercises/L19_TA_Exercises/app/src/main/java/com/example/l19securitymodeldemo/MainActivity.kt
@@ -0,0 +1,47 @@
+package com.example.l19securitymodeldemo
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import com.example.l19securitymodeldemo.ui.theme.L19SecurityModelDemoTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+            L19SecurityModelDemoTheme {
+                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
+                    Greeting(
+                        name = "Android",
+                        modifier = Modifier.padding(innerPadding)
+                    )
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun Greeting(name: String, modifier: Modifier = Modifier) {
+    Text(
+        text = "Hello $name!",
+        modifier = modifier
+    )
+}
+
+@Preview(showBackground = true)
+@Composable
+fun GreetingPreview() {
+    L19SecurityModelDemoTheme {
+        Greeting("Android")
+    }
+}
