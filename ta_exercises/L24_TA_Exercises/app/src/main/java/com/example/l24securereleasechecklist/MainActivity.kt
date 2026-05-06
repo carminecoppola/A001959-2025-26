9a2270b9beac432006015b50054d3b0d896435b1
vincenzobucciero <vincenzo.bucciero001@studenti.uniparthenope.it>
2026-05-06 11:43:41 +0200

    feat: Initialize L24 Secure Release Checklist app with essential resources and layout
    - Added vector drawables for launcher background and foreground.
    - Created main activity layout with buttons for various checklist actions.
    - Defined adaptive icons for different screen densities.
    - Included color resources and string resources for app name.
    - Established themes for the app.
    - Set up XML files for backup and data extraction rules.
    - Configured network security settings.
    - Added unit test example.
    - Created build configuration files including Gradle settings and dependencies.
    - Implemented Gradle wrapper scripts for project execution.

---
+++ ta_exercises/L24_TA_Exercises/app/src/main/java/com/example/l24securereleasechecklist/MainActivity.kt
@@ -0,0 +1,47 @@
+package com.example.l24securereleasechecklist
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import com.example.l24securereleasechecklist.ui.theme.L24SecureReleaseChecklistTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+            L24SecureReleaseChecklistTheme {
+                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
+                    Greeting(
+                        name = "Android",
+                        modifier = Modifier.padding(innerPadding)
+                    )
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun Greeting(name: String, modifier: Modifier = Modifier) {
+    Text(
+        text = "Hello $name!",
+        modifier = modifier
+    )
+}
+
+@Preview(showBackground = true)
+@Composable
+fun GreetingPreview() {
+    L24SecureReleaseChecklistTheme {
+        Greeting("Android")
+    }
+}
---
+++ ta_exercises/L24_TA_Exercises/app/src/main/java/com/example/l24securereleasechecklist/ReleaseCheck.kt
@@ -0,0 +1,4 @@
+package com.example.l24securereleasechecklist
+
+class ReleaseCheck {
+}
